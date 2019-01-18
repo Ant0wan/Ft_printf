6928f4fcd9a909f4bda97d307dc964e5e6ab567a
Ant0wan <abarthel@student.42.fr>
2019-01-18 13:40:51 +0100

    tests printf

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/16 16:48:11 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/18 13:04:15 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/01/18 13:40:39 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,16 @@ void courses(char* arg0, ...)
 
 int main(void)
 {
-	courses("List is made of:", "oeufs", "chocolat", "sucre", "farine", NULL);
+//	courses("List is made of:", "oeufs", "chocolat", "sucre", "farine", NULL);
+
+	int number = 42;
+
+	printf("test1 %#x\n", 42);
+    printf("test2 %-10d \n", number);
+    printf("test3 %010d \n", number);
+    printf("test4 %-#10x \n", number);  
+    printf("test5 %#x \n", number);	
+
+
 	return 0;
 }
