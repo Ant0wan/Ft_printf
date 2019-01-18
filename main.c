ded915843ffc25dac205600e7e9a2a5cd18ee057
Antoine BARTHELEMY <abarthel@e2r5p10.42.fr>
2019-01-18 15:12:11 +0100

    tests printf

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/16 16:48:11 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/18 15:10:42 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/01/18 15:12:01 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,43 +38,43 @@ int main(void)
 
 	/* FLAGS */
 	int num = 42;
-	printf("test1 %#x\n", 42);
-	printf("test2 %-10d \n", num);
-	printf("test2 %010d \n", num);
-	printf("test2 %0d \n", num);
-	printf("test2 %+10d \n", num);
-	printf("test2 %0+10d \n", num);
-	printf("test2 %-10d \n", num * (-1));
-	printf("test2 %-10d \n", num * (-1));
-	printf("test2 %+10d \n", num * (-1));
-	printf("test2 %0+10d \n", num * (-1));
-	printf("test3 %010d \n", num);
-	printf("test4 %-#10x \n", num);  
-	printf("test4 %-#10x \n", num);  
-	printf("test4 %#10x \n", num);  
-	printf("test4 %0#10x \n", num);  
+	printf("test 1 %#x\n", 42);
+	printf("test 2 %-10d \n", num);
+	printf("test 3 %010d \n", num);
+	printf("test 4 %0d \n", num);
+	printf("test 5 %+10d \n", num);
+	printf("test 6 %0+10d \n", num);
+	printf("test 7 %-10d \n", num * (-1));
+	printf("test 8 %-10d \n", num * (-1));
+	printf("test 9 %+10d \n", num * (-1));
+	printf("test10 %0+10d \n", num * (-1));
+	printf("test11 %010d \n", num);
+	printf("test12 %-#10x \n", num);  
+	printf("test13 %-#10x \n", num);  
+	printf("test14 %#10x \n", num);  
+	printf("test15 %0#10x \n", num);  
 //	printf("test4 %+#10x \n", num);  
 //	printf("test4 %-+#10x \n", num);  
-	printf("test5 %#x \n", num);	
+	printf("test16 %#x \n", num);	
 
 	/* FIELD WIDTH*/
 	char *pointer = "little";
 	//printf("test fail%4 %4d%10s\n", num, pointer);
-	printf("test6 %4d%10s\n", num, pointer);
-	printf("test7 %1d%1s\n", num, pointer);
-	printf("test8 %*d\n", 42, num);
+	printf("test17 %4d%10s\n", num, pointer);
+	printf("test18 %1d%1s\n", num, pointer);
+	printf("test19 %*d\n", 42, num);
 	
 	/* PRECISION */
 	double dd = 42.123456789;
-	printf("test9 %*.2f\n", 42, dd);
-	printf("test10 %8.2f\n", dd);
-	printf("test11 %.2f\n", dd);
-	printf("test12 %.42f\n", dd);
-	printf("test13 %.f\n", dd);
-	printf("test14 %.*f\n", 8, dd);
-	printf("test14 %*.*f\n", 100, 42, dd);
+	printf("test20 %*.2f\n", 42, dd);
+	printf("test21 %8.2f\n", dd);
+	printf("test22 %.2f\n", dd);
+	printf("test23 %.42f\n", dd);
+	printf("test24 %.f\n", dd);
+	printf("test25 %.*f\n", 8, dd);
+	printf("test26 %*.*f\n", 100, 42, dd);
 
-	printf(" %%");
+	printf("test27 %%");
 	
 
 	return 0;
