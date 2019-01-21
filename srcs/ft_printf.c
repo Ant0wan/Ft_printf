61947ed8860aae951e540b37a346da8c2ccf1ec4
Ant0wan <abarthel@student.42.fr>
2019-01-21 16:12:22 +0100

    on the go

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/21 14:36:30 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/21 14:37:24 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/01/21 16:12:05 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 int	ft_printf(const char *restrict format, ...)
 {
-	return ();
+	va_list ap;
+	void *str;
+
+	va_start(ap, format);
+
+	str = va_arg(ap, int);
+	
+	va_end(ap);
+
+	return (0);
 }
