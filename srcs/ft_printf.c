14a244d6b655fd1af30c36733f82924e94ecc285
Antoine BARTHELEMY <abarthel@e1r2p16.42.fr>
2019-01-23 13:00:14 +0100

    added libft ?

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/22 14:27:49 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/22 14:28:46 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/01/22 14:51:22 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,14 @@
 
 int		printf(const char * restrict format, ...)
 {
+	va_list ap;
+
+	va_start(ap, format);
+	
+	printf("%s", format);
+	printf("%s", va_arg(ap, char*));
+
+	va_end(ap);
+
 	return (0);
 }
