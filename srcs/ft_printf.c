b5d171b4e0f51970f641f96d996474bd0a2d2eed
Ant0wan <abarthel@student.42.fr>
2019-01-23 17:00:51 +0100

    updte for makefile

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/22 14:27:49 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/22 14:51:22 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/01/23 17:00:39 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int		printf(const char * restrict format, ...)
+int		ft_printf(const char * restrict format, ...)
 {
 	va_list ap;
 
