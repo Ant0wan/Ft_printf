78084ccfe56f1b3f0146b7b0140f7ceb39510edf
Ant0wan <abarthel@student.42.fr>
2019-01-24 14:55:58 +0100

    dispacther on the go hash table

--- srcs/dispatcher.c
+++ srcs/dispatcher.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/24 12:37:47 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/24 12:57:31 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/01/24 14:52:36 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 t_typeop	g_typeoptab[] =
 {
-	{ "d", (void*)&ft_itoa},
-	{ "\0", (void*)0}
+	{ "s", (void*)&ft_putstr},
+	{ "d", (void*)&ft_itoa}
 };
+
+void	*dispatcher(char *str)
+{
+	return ((void*)str);
+}
