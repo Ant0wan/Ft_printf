b01b189804b615e6ae3aa68021010d7166a96757
Ant0wan <abarthel@student.42.fr>
2019-01-24 12:56:44 +0100

    ok for the tab, trying in const

--- main.c
+++ main.c
@@ -1,34 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/22 14:27:04 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/24 12:37:10 by abarthel         ###   ########.fr       */
+/*   Created: 2019/01/23 17:35:29 by abarthel          #+#    #+#             */
+/*   Updated: 2019/01/24 12:56:16 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-# include "libft.h"
-# include <stdarg.h>
-
-# include <stdio.h> // DEBUGGING
-
-typedef struct	s_typeop
+int	main()
 {
-	char	*type;
-	void	*(*f)();
-}				t_typeop;
-
-extern t_typeop	g_typeoptab[]; // =
-//{
-//	{ "\0", (void*)0}
-//};
-
-int				ft_printf(const char *restrict format, ...);
-
-#endif
+	printf("main: %s", g_typeoptab[0].f(42));
+	ft_printf("Hello World\n");
+	return (0);
+}
--- srcs/dispatcher.c
+++ srcs/dispatcher.c
@@ -1,34 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   disptacher.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/22 14:27:04 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/24 12:37:10 by abarthel         ###   ########.fr       */
+/*   Created: 2019/01/24 12:37:47 by abarthel          #+#    #+#             */
+/*   Updated: 2019/01/24 12:51:09 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "ft_printf.h"
 
-# include "libft.h"
-# include <stdarg.h>
-
-# include <stdio.h> // DEBUGGING
-
-typedef struct	s_typeop
+t_typeop	g_typeoptab[] =
 {
-	char	*type;
-	void	*(*f)();
-}				t_typeop;
-
-extern t_typeop	g_typeoptab[]; // =
-//{
-//	{ "\0", (void*)0}
-//};
-
-int				ft_printf(const char *restrict format, ...);
-
-#endif
+	{ "d", (void*)&ft_itoa},
+	{ "\0", (void*)0}
+};
