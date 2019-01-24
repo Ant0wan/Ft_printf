981c7f4c0f5476b08f171900901ac32cb020f342
Antoine BARTHELEMY <abarthel@e1r5p11.42.fr>
2019-01-24 19:49:03 +0100

    go for parsing on int and str first

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/22 14:27:49 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/24 19:42:56 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/01/24 19:48:15 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,20 @@
 int		ft_printf(const char * restrict format, ...)
 {
 	va_list ap;
-
+	
 	va_start(ap, format);
 	va_end(ap);
 	ft_putstr("here s:");
-	dispatcher("s");
+	ft_putnbr((int)dispatcher("s"));
 	ft_putstr("\n");
 	ft_putstr("here d:");
-	dispatcher("d");
+	ft_putnbr((int)dispatcher("d"));
 	ft_putstr("\n");
 	ft_putstr("here X:");
-	dispatcher("X");
+	ft_putnbr((int)dispatcher("X"));
 	ft_putstr("\n");
 	ft_putstr("here 0:");
-	dispatcher("0");
+	ft_putnbr((int)dispatcher("0"));
 	ft_putstr("\n");
 	return (0);
 }
