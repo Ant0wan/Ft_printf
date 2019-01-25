11f61881680a451fcdfffb63e273632b26da60f9
Antoine BARTHELEMY <abarthel@e2r7p16.42.fr>
2019-01-25 14:14:22 +0100

    dispatcher works

--- srcs/dispatcher.c
+++ srcs/dispatcher.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/24 12:37:47 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/24 19:45:11 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/01/25 14:03:36 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ t_typeop	g_typeoptab[] =
 {
 	{ "s", (void*)&ft_putstr},
 	{ "d", (void*)&ft_itoa},
+	{ "ll", (void*)&ft_itoa},
 	{ "\0", (void*)0}
 };
 
@@ -24,9 +25,12 @@ void		*dispatcher(char *str)
 	int	i;
 
 	i = 0;
-	while (g_typeoptab[i].type[0] && g_typeoptab[i].type[0] != *str)
+	while (g_typeoptab[i].type[0] && ft_strcmp(g_typeoptab[i].type, str))
 		++i;
-	if (g_typeoptab[i].type[0] && g_typeoptab[i].type[0] == *str)
-		return(g_typeoptab[i].f);
+	if (g_typeoptab[i].type[0])
+	{
+			ft_putnbr((int)g_typeoptab[i].f);
+			return(g_typeoptab[i].f);
+	}
 	return ((void*)0);
 }
