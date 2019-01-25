750c2bcd2fba9b70e612f27cb96d3af70b5a8468
Antoine BARTHELEMY <abarthel@e2r7p16.42.fr>
2019-01-25 15:39:08 +0100

    dispatcher on the go for tests

--- srcs/dispatcher.c
+++ srcs/dispatcher.c
@@ -6,16 +6,53 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/24 12:37:47 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/25 14:03:36 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/01/25 15:35:30 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+t_typeop	g_modifiers[] =
+{
+	{ " ", (void*)0},
+	{ "+", (void*)0},
+	{ "-", (void*)0},
+	{ "#", (void*)0},
+	{ "0", (void*)0},
+	{ "\'", (void*)0},
+	{ "I", (void*)0},
+	{ "\0", (void*)0}
+};
+
+t_typeop	g_conversions[] =
+{
+	{ "o", (void*)0},
+	{ "x", (void*)0},
+	{ "X", (void*)0},
+	{ "e", (void*)0},
+	{ "E", (void*)0},
+	{ "g", (void*)0},
+	{ "G", (void*)0},
+	{ "\0", (void*)0}
+};
+
 t_typeop	g_typeoptab[] =
 {
-	{ "s", (void*)&ft_putstr},
-	{ "d", (void*)&ft_itoa},
+	{ "c", (void*)&ft_wputchar},	 // char && unsigned char		1 bytes
+	{ "hd", (void*)0},				 // short int					2 bytes
+	{ "hu", (void*)0},				 // unsigned short int			2 bytes
+	{ "u", (void*)0},				 // unsigned int				4 bytes
+	{ "u", (void*)0},				 // unsigned int				4 bytes
+	{ "f", (void*)0},				 // float						4 bytes
+	{ "d", (void*)&ft_itoa},		 // int							4 bytes
+	{ "i", (void*)&ft_itoa},		 // int							4 bytes
+	{ "ld", (void*)&ft_itoa},		 // long int					4 bytes
+	{ "lu", (void*)&ft_itoa},		 // unsigned long int			4 bytes
+	{ "lld", (void*)&ft_itoa},		 // long long int				8 bytes
+	{ "llu", (void*)&ft_itoa},		 // unsigned long long int		8 bytes
+	{ "lf", (void*)0},				 // double						8 bytes
+	{ "Lf", (void*)0},				 // long double				   12 bytes
+	{ "s", (void*)&ft_putstr},		 // char pointer				8 bytes
 	{ "ll", (void*)&ft_itoa},
 	{ "\0", (void*)0}
 };
@@ -28,9 +65,6 @@ void		*dispatcher(char *str)
 	while (g_typeoptab[i].type[0] && ft_strcmp(g_typeoptab[i].type, str))
 		++i;
 	if (g_typeoptab[i].type[0])
-	{
-			ft_putnbr((int)g_typeoptab[i].f);
 			return(g_typeoptab[i].f);
-	}
 	return ((void*)0);
 }
--- srcs/ft_wputchar.c
+++ srcs/ft_wputchar.c
@@ -1,39 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_wputchar.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/22 14:27:04 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/25 15:36:09 by abarthel         ###   ########.fr       */
+/*   Created: 2018/11/12 15:49:24 by abarthel          #+#    #+#             */
+/*   Updated: 2019/01/25 15:37:23 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
+#include "libft.h"
 
-# include "libft.h"
-# include <stdarg.h>
-
-# include <stdio.h> // DEBUGGING
-
-typedef struct	s_typeop
+void	ft_wputchar(wchar_t c)
 {
-	char	*type;
-	void	*(*f)();
-}				t_typeop;
-
-extern t_typeop	g_modifiers[];
-
-extern t_typeop	g_conversions[];
-
-extern t_typeop	g_typeoptab[];
-
-void			ft_wputchar(wchar_t c);
-
-int				ft_printf(const char *restrict format, ...);
-
-void			*dispatcher(char *str);
-
-#endif
+	write(1, &c, 1);
+}
