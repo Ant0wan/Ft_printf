c154fb5a4ae62a5e7dd92f4bf5b2924c7d4bdde7
Antoine BARTHELEMY <abarthel@e2r9p3.42.fr>
2019-01-29 18:45:55 +0100

    fputwc + fputws + putwchar ok

--- libftwchar/ft_fputws.c
+++ libftwchar/ft_fputws.c
@@ -1,26 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_wchar.h                                         :+:      :+:    :+:   */
+/*   ft_fputws.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/29 15:19:44 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/29 18:37:20 by abarthel         ###   ########.fr       */
+/*   Created: 2019/01/29 18:25:44 by abarthel          #+#    #+#             */
+/*   Updated: 2019/01/29 18:44:47 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_WCHAR_H
-#define FT_WCHAR_H
+#include "ft_wchar.h"
 
 #include <stdlib.h>
 
-int	ft_wcscmp(const wchar_t *s1, const wchar_t *s2);
+#define RET_ERROR -1
 
-int	ft_fputwc(wchar_t wc, int fd);
+int	ft_fputws(const wchar_t *restrict ws, int fd)
+{
+	int		ret;
+	size_t	i;
 
-int	ft_fputws(const wchar_t *restrict ws, int fd);
-
-int	ft_putwchar(wchar_t wc);
-
-#endif
+	i = 0;
+	if (ws && *ws)
+	{
+		while (ws[i])
+		{
+			if ((ret = ft_fputwc(ws[i], fd)) == RET_ERROR)
+				return (ret);
+			else
+				++i;
+		}
+		return (ret);
+	}
+	return (RET_ERROR);
+}
