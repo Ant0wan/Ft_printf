20d0456fc446cb8d833be8840ec0249e08746ff8
Ant0wan <abarthel@student.42.fr>
2019-01-29 18:49:48 +0100

    norminette

--- libftwchar/dispatcher/dispatcher.h
+++ libftwchar/dispatcher/dispatcher.h
@@ -6,14 +6,14 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 16:06:04 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/29 16:43:48 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/01/29 18:47:19 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DISPATCHER_H
 # define DISPATCHER_H
 
-#include <stdlib.h>
+# include <stdlib.h>
 
 typedef struct	s_operator
 {
