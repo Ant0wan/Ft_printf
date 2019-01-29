34f06bc229ae6ec637c38cea31c659d478851398
Antoine BARTHELEMY <abarthel@e2r9p3.42.fr>
2019-01-29 19:10:54 +0100

    done for today, still to utf8 and then start cast for fwprint from printf then start parsing

--- libftprintf/ft_printf.c
+++ libftprintf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/22 14:27:49 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/29 16:39:27 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/01/29 19:09:38 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int		ft_printf(const char *restrict format, ...)
 	int		ret;
 
 	va_start(ap, format);
+
+	// format in fwprintf is not char but wchar_t carefull to treat properly !!
 	ret = ft_fwprintf(STDOUT_FILENO, format, ap);
 	va_end(ap);
 	return (ret);
