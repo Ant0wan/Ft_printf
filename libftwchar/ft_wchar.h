fbf723ee134cd1ff143c3db4d5f30a4ee87fa450
Antoine BARTHELEMY <abarthel@e2r9p3.42.fr>
2019-01-29 18:22:51 +0100

    fputwc

--- libftwchar/ft_wchar.h
+++ libftwchar/ft_wchar.h
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/29 15:19:44 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/29 16:46:36 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/01/29 18:22:35 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,7 @@
 #define FT_WCHAR_H
 
 int	ft_wcscmp(const wchar_t *s1, const wchar_t *s2);
+
+int	ft_fputwc(wchar_t wc, int fd);
 
 #endif
