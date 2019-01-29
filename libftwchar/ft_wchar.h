0dae5060933f86f1ad762e7d179608f5418e4608
Ant0wan <abarthel@student.42.fr>
2019-01-29 16:46:53 +0100

    wchar on the go

--- libftwchar/ft_wchar.h
+++ libftwchar/ft_wchar.h
@@ -1,22 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   dispatcher.h                                       :+:      :+:    :+:   */
+/*   ft_wchar.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/28 16:06:04 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/29 11:07:16 by abarthel         ###   ########.fr       */
+/*   Created: 2019/01/29 15:19:44 by abarthel          #+#    #+#             */
+/*   Updated: 2019/01/29 16:46:36 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef DISPATCHER_H
-# define DISPATCHER_H
+#ifndef FT_WCHAR_H
+#define FT_WCHAR_H
 
-typedef struct	s_operator
-{
-	wchar_t	*type;
-	void	*(*f)();
-}				t_operator;
+int	ft_wcscmp(const wchar_t *s1, const wchar_t *s2);
 
 #endif
--- libftwchar/ft_wprintf.c
+++ libftwchar/ft_wprintf.c
@@ -6,11 +6,13 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 15:28:13 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/28 16:04:21 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/01/29 15:03:45 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_wprintf.h"
+
+#include <stdarg.h>
 
 int		ft_wprintf(const wchar_t *restrict format, ...)
 {
