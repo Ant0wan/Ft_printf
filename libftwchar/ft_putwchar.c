d9527193d79cec8ec554eff093e2e94102f0b9e1
Ant0wan <abarthel@student.42.fr>
2019-02-01 16:00:54 +0100

    ok norme

--- libftwchar/ft_putwchar.c
+++ libftwchar/ft_putwchar.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/29 17:03:32 by abarthel          #+#    #+#             */
-/*   Updated: 2019/02/01 15:44:55 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/02/01 16:00:22 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,35 +15,28 @@
 
 #include "utf8.h"
 
-#define RET_ERROR         -1
-#define UTF8ENCODING_SIZE  4
+#define RET_ERROR      -1
+#define UTF8ENCOD_SIZE  4
 
 int	ft_putwchar(wchar_t wc)
 {
-	int		ret;
 	char	buf[4];
 
 	if (wc >= 0x00)
 	{
 		if (wc <= 0x007F)
-		{
-			if ((ret = write(STDOUT_FILENO, &wc, sizeof(wc))) > -1)
-				return ((int)wc);
-			else
-				return (ret);
-		}
+			return ((write(STDOUT_FILENO, &wc, sizeof(wc))) == RET_ERROR ?
+					RET_ERROR : (int)wc);
 		else
 		{
 			if (utf8_encoder(&wc) == RET_ERROR)
 				return (RET_ERROR);
-			buf[0] = wc  >> (3 * 8);
-			buf[1] = wc  >> (2 * 8);
-			buf[2] = wc  >> (1 * 8);
+			buf[0] = wc >> 24;
+			buf[1] = wc >> 16;
+			buf[2] = wc >> 8;
 			buf[3] = wc;
-			if ((ret = write(STDOUT_FILENO, buf, UTF8ENCODING_SIZE)) > -1)
-				return ((int)wc);
-			else
-				return (ret);
+			return ((write(STDOUT_FILENO, buf, UTF8ENCOD_SIZE)) == RET_ERROR ?
+					RET_ERROR : (int)wc);
 		}
 	}
 	else
