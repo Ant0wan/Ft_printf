8862a8e2cd164adb17488a8ea436e3d7f354d813
Antoine BARTHELEMY <abarthel@e1r4p10.42.fr>
2019-02-04 14:58:58 +0100

    attribute added to .h

--- libftprintf/ft_printf.c
+++ libftprintf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/22 14:27:49 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/29 19:09:38 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/02/04 12:50:27 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@ int		ft_printf(const char *restrict format, ...)
 	va_start(ap, format);
 
 	// format in fwprintf is not char but wchar_t carefull to treat properly !!
-	ret = ft_fwprintf(STDOUT_FILENO, format, ap);
+//	ret = ft_fwprintf(STDOUT_FILENO, format, ap);
+	ret = 1;
 	va_end(ap);
 	return (ret);
 }
--- libftprintf/ft_printf.h
+++ libftprintf/ft_printf.h
@@ -6,13 +6,14 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/22 14:27:04 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/29 15:19:22 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/02/04 14:57:38 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-int	ft_printf(const char *restrict format, ...);
+int	__attribute__((format(printf, 1, 2)))
+	ft_printf(const char *restrict format,...);
 
 #endif
