409c8171c3bde36de4805fe730ea3fa728654d68
Ant0wan <abarthel@student.42.fr>
2019-02-05 16:08:26 +0100

    all added

--- libftprintf/ft_printf.h
+++ libftprintf/ft_printf.h
@@ -6,14 +6,14 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/22 14:27:04 by abarthel          #+#    #+#             */
-/*   Updated: 2019/02/04 14:57:38 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/02/05 16:05:55 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-int	__attribute__((format(printf, 1, 2)))
-	ft_printf(const char *restrict format,...);
+int	ft_printf(const char *restrict format, ...) __attribute__
+	((format(printf,1,2)));
 
 #endif
