5a1a0594bc26920dedbaa998d0c4a1b10768ffe3
Ant0wan <abarthel@student.42.fr>
2019-02-06 18:40:25 +0100

    adding makefiles

--- libftwchar/ft_fwprintf.c
+++ libftwchar/ft_fwprintf.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/28 15:35:34 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/29 19:09:52 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/02/06 18:35:16 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,8 @@
 
 int	ft_fwprintf(int fd, const wchar_t *restrict format, ...)
 {
+	(void)fd;
+	(void)format;
+
 	return (0);
 }
