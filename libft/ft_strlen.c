94ce397e36c7540f7d577f31ccd35a0adf8e789a
Antoine BARTHELEMY <abarthel@e1r2p6.42.fr>
2019-02-06 11:57:43 +0100

    added attributes

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 13:35:20 by abarthel          #+#    #+#             */
-/*   Updated: 2019/01/29 12:50:23 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/02/06 11:44:53 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,18 @@ size_t		ft_strlen(const char *s)
 	while (s[len])
 		++len;
 	return (len);
+}
+
+int strlen_my(const char *s)
+{
+	int len = 0;
+	for(;;)
+	{
+		unsigned x = *(unsigned*)s;
+		if((x & 0xFF) == 0) return len;
+		if((x & 0xFF00) == 0) return len + 1;
+		if((x & 0xFF0000) == 0) return len + 2;
+		if((x & 0xFF000000) == 0) return len + 3;
+		s += 4, len += 4;
+	}
 }
