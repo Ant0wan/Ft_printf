4377eff48f11b3cbe4b5df8092a2ee6cbc228fb3
Ant0wan <abarthel@student.42.fr>
2019-02-06 11:57:43 +0100

    added attributes

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 11:55:45 by abarthel          #+#    #+#             */
-/*   Updated: 2019/02/06 11:35:20 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/02/06 11:57:29 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 
 # include <stdlib.h>
 
-void			ft_bzero(void *s, size_t n);
+void			ft_bzero(void *s, size_t n) __attribute__
+				((nonnull(1)));
 
 int				ft_isalpha(int c);
 
@@ -31,52 +32,81 @@ int				ft_toupper(int c);
 
 int				ft_tolower(int c);
 
-int				ft_atoi(const char *str);
+int				ft_atoi(const char *str) __attribute__
+				((nonnull(1)));
 
-int				ft_strcmp(const char *s1, const char *s2);
+int				ft_strcmp(const char *s1, const char *s2) __attribute__
+				((nonnull(1,2)));
 
-int				ft_strncmp(const char *s1, const char *s2, size_t n);
+int				ft_strncmp(const char *s1, const char *s2,
+		size_t n) __attribute__
+				((nonnull(1,2)));
 
-int				ft_memcmp(const void *s1, const void *s2, size_t n);
+int				ft_memcmp(const void *s1, const void *s2,
+		size_t n) __attribute__
+				((nonnull(1,2)));
 
-size_t			ft_strlen(const char *s);
+size_t			ft_strlen(const char *s) __attribute__
+				((nonnull(1)));
 
-size_t			ft_strlcat(char *dst, const char *src, size_t size);
+size_t			ft_strlcat(char *dst, const char *src,
+		size_t size) __attribute__
+				((nonnull(1,2)));
 
-void			*ft_memset(void	*b, int c, size_t len);
+void			*ft_memset(void	*b, int c, size_t len) __attribute__
+				((nonnull(1)));
 
-void			*ft_memcpy(void *dst, const void *src, size_t n);
+void			*ft_memcpy(void *dst, const void *src, size_t n) __attribute__
+				((nonnull(1,2)));
 
-void			*ft_memccpy(void *dst, const void *src, int c, size_t n);
+void			*ft_memccpy(void *dst, const void *src, int c,
+		size_t n) __attribute__
+				((nonnull(1,2)));
 
-void			*ft_memmove(void *dst, const void *src, size_t len);
+void			*ft_memmove(void *dst, const void *src,
+		size_t len) __attribute__
+				((nonnull(1,2)));
 
-void			*ft_memchr(const void *s, int c, size_t n);
+void			*ft_memchr(const void *s, int c, size_t n) __attribute__
+				((nonnull(1)));
 
-char			*ft_strstr(const char *haystack, const char *needle);
+char			*ft_strstr(const char *haystack, const char
+		*needle) __attribute__
+				((nonnull(1,2)));
 
-char			*ft_strcat(char *s1, const char *s2);
+char			*ft_strcat(char *s1, const char *s2) __attribute__
+				((nonnull(1,2)));
 
-char			*ft_strncat(char *s1, const char *s2, size_t n);
+char			*ft_strncat(char *s1, const char *s2, size_t n) __attribute__
+				((nonnull(1,2)));
 
-char			*ft_strcpy(char *dst, const char *src);
+char			*ft_strcpy(char *dst, const char *src) __attribute__
+				((nonnull(1,2)));
 
-char			*ft_strncpy(char *dst, const char *src, size_t len);
+char			*ft_strncpy(char *dst, const char *src,
+		size_t len) __attribute__
+				((nonnull(1,2)));
 
-char			*ft_strdup(const char *s1);
+char			*ft_strdup(const char *s1) __attribute__
+				((nonnull(1)));
 
-char			*ft_strndup(const char *s1, size_t len);
+char			*ft_strndup(const char *s1, size_t len) __attribute__
+				((nonnull(1)));
 
 char			*ft_strnstr(const char *haystack, const char *neddle,
-		size_t len);
+		size_t len) __attribute__
+				((nonnull(1,2)));
 
-char			*ft_strchr(const char *s, int c);
+char			*ft_strchr(const char *s, int c) __attribute__
+				((nonnull(1)));
 
-char			*ft_strrchr(const char *s, int c);
+char			*ft_strrchr(const char *s, int c) __attribute__
+				((nonnull(1)));
 
 void			ft_memdel(void **ap);
 
-void			ft_strdel(char **as);
+void			ft_strdel(char **as) __attribute__
+				((nonnull(1)));
 
 void			ft_strclr(char *s);
 
@@ -88,7 +118,8 @@ void			ft_putchar(char c) __attribute__
 				((noreturn));
 
 void			ft_putstr(char const *s) __attribute__
-				((noreturn));
+				((noreturn)) __attribute__
+				((nonnull(1)));
 
 void			ft_putendl(char const *s) __attribute__
 				((noreturn));
