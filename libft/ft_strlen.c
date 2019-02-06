e001db8c148b3a0471790f4a768f148fdffa7f3d
Antoine BARTHELEMY <abarthel@e1r2p6.42.fr>
2019-02-06 12:41:43 +0100

    strlen with bitwise op

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,32 +6,35 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 13:35:20 by abarthel          #+#    #+#             */
-/*   Updated: 2019/02/06 11:44:53 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/02/06 12:41:25 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
+#define FIRST_BYTE 0b11111111
+#define SECOND_BYTE 0b1111111100000000
+#define THIRD_BYTE	0b111111110000000000000000
+#define FOURTH_BYTE	0b11111111000000000000000000000000
+
 size_t		ft_strlen(const char *s)
 {
-	size_t	len;
+	size_t			len;
+	unsigned int	next4bytes;
 
 	len = 0;
-	while (s[len])
-		++len;
-	return (len);
-}
-
-int strlen_my(const char *s)
-{
-	int len = 0;
-	for(;;)
+	while (42)
 	{
-		unsigned x = *(unsigned*)s;
-		if((x & 0xFF) == 0) return len;
-		if((x & 0xFF00) == 0) return len + 1;
-		if((x & 0xFF0000) == 0) return len + 2;
-		if((x & 0xFF000000) == 0) return len + 3;
-		s += 4, len += 4;
+		next4bytes = *(unsigned int*)s;
+		if ((next4bytes & FIRST_BYTE) == 0)
+			return (len);
+		else if ((next4bytes & SECOND_BYTE) == 0)
+			return (len + 1);
+		else if ((next4bytes & THIRD_BYTE) == 0)
+			return (len + 2);
+		else if ((next4bytes & FOURTH_BYTE) == 0)
+			return (len + 3);
+		s += 4;
+		len += 4;
 	}
 }
