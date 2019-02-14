142d5bf7bd52b30a4b0e74b60519822a76bf3f42
Antoine BARTHELEMY <abarthel@e1r1p1.42.fr>
2019-02-14 13:52:42 +0100

    header ok

--- libftprintf/ft_vasprintf.c
+++ libftprintf/ft_vasprintf.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_vasprintf.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: abarthel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 16:13:29 by abarthel          #+#    #+#             */
-/*   Updated: 2019/02/13 17:37:40 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/02/14 13:43:12 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,14 @@
 
 #include <stdio.h> // debugging
 
+#include "libft.h"
+
 int	ft_vasprintf(char **ret, const char *format, va_list ap)
 {
-	return (vasprintf(ret, format, ap));
+	int	nb_bytes;
+
+	ft_putstr(format);
+	ft_putnbr(va_arg(ap, int));
+	
+	return (nb_bytes);
 }
