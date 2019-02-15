04a0b9c758a7990fe234e04582afee6a793eff31
Ant0wan <abarthel@student.42.fr>
2019-02-15 16:21:04 +0100

    adding dispatcher

--- libftprintf/ft_printf_prs.c
+++ libftprintf/ft_printf_prs.c
@@ -1,20 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_fwprintf_disp.h                                 :+:      :+:    :+:   */
+/*   ft_printf_prs.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/28 16:06:04 by abarthel          #+#    #+#             */
-/*   Updated: 2019/02/06 18:41:33 by abarthel         ###   ########.fr       */
+/*   Created: 2019/02/15 12:39:43 by abarthel          #+#    #+#             */
+/*   Updated: 2019/02/15 14:23:04 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_FWPRINTF_DISP_H
-# define FT_FWPRINTF_DISP_H
+/*
+** Printf parsing
+*/
 
-# include <stdlib.h>
 
-void	*dispatcher(wchar_t *str);
-
-#endif
--- libftprintf/ft_vasprintf.c
+++ libftprintf/ft_vasprintf.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 16:13:29 by abarthel          #+#    #+#             */
-/*   Updated: 2019/02/14 13:43:12 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/02/15 14:43:47 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,15 @@
 
 #include "libft.h"
 
+#define BUFF 1024
+
 int	ft_vasprintf(char **ret, const char *format, va_list ap)
 {
-	int	nb_bytes;
-
-	ft_putstr(format);
-	ft_putnbr(va_arg(ap, int));
+	(void)ret;
+	(void)format;
+	(void)ap;
+	if (!(ret = (char**)ft_memalloc(sizeof(**ret) * BUFF + 1)))
+		return (-1);
 	
-	return (nb_bytes);
+	return (2);
 }
