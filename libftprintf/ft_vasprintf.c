24f0d5ef35b99552d63be4fb093140a6f65b7dad
Ant0wan <abarthel@student.42.fr>
2019-02-15 18:45:01 +0100

    ok for dispatcher

--- libftprintf/ft_vasprintf.c
+++ libftprintf/ft_vasprintf.c
@@ -6,16 +6,13 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/13 16:13:29 by abarthel          #+#    #+#             */
-/*   Updated: 2019/02/15 14:43:47 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/02/15 18:05:31 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 
 #include "ft_vdprintf.h"
-
-#include <stdio.h> // debugging
-
 #include "libft.h"
 
 #define BUFF 1024
@@ -27,6 +24,5 @@ int	ft_vasprintf(char **ret, const char *format, va_list ap)
 	(void)ap;
 	if (!(ret = (char**)ft_memalloc(sizeof(**ret) * BUFF + 1)))
 		return (-1);
-	
 	return (2);
 }
