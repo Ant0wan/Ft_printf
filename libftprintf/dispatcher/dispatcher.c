42f7190b52fe389e74a9dd80f2e44375953fe643
Antoine BARTHELEMY <abarthel@e1r1p6.42.fr>
2019-02-15 16:52:05 +0100

    ok for build

--- libftprintf/dispatcher/dispatcher.c
+++ libftprintf/dispatcher/dispatcher.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 14:51:32 by abarthel          #+#    #+#             */
-/*   Updated: 2019/02/15 16:44:13 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/02/15 16:50:55 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,12 +28,12 @@ static const t_disp	g_disp[] =
 	{ "d", NULL}
 };
 
-void				*dispatcher(char *str)
+void				*dispatcher(void *str)
 {
 	char	i;
 
 	i = 0;
-	while (i < NB_PTR && ft_strcmp(g_specifier[(int)i].type, str))
+	while (i < NB_PTR && ft_strcmp(g_disp[(int)i].type, str))
 		++i;
 	if (g_disp[(int)i].type[0])
 		return (g_disp[(int)i].f);
