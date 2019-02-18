099735fa7307820ac58d86cf7a278b7440e821cd
Antoine BARTHELEMY <abarthel@e1r2p12.42.fr>
2019-02-18 16:57:30 +0100

    added putnbr base

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 11:55:45 by abarthel          #+#    #+#             */
-/*   Updated: 2019/02/06 17:11:53 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/02/18 16:56:46 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -109,6 +109,8 @@ void			ft_putstr(char const *s);
 void			ft_putendl(char const *s);
 
 void			ft_putnbr(int n);
+
+void			ft_putnbr_base(int nbr, char *base);
 
 void			ft_putchar_fd(char c, int fd);
 
