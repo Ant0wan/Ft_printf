fd1649f60c018cf728c3f5e00dea222cb06c683c
Ant0wan <abarthel@student.42.fr>
2019-02-21 18:18:22 +0100

    protected ft_memalloc with error handling

--- libftprintf/valist_cpy.c
+++ libftprintf/valist_cpy.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 17:39:02 by abarthel          #+#    #+#             */
-/*   Updated: 2019/02/21 18:12:36 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/02/21 18:17:16 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,15 @@
 
 #include "libft.h"
 
+#define RET_ERROR -1
+
 t_ap	g_ap;
 
 int	valist_cpy(va_list ap, va_list **array)
 {
 	(void)ap;
 	(void)array;
-	*array = (va_list*)ft_memalloc(sizeof(va_list) * g_ap.nb_ap);
+	if (!(*array = (va_list*)ft_memalloc(sizeof(va_list) * g_ap.nb_ap)))
+		return (RET_ERROR);
 	return (0);
 }
