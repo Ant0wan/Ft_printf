87a2456c2e8f49e46cf9000e357d75351838edae
Antoine BARTHELEMY <abarthel@e1r4p8.42.fr>
2019-02-21 12:13:18 +0100

    adding wrapper...

--- libftprintf/ft_printf_prs.c
+++ libftprintf/ft_printf_prs.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 12:39:43 by abarthel          #+#    #+#             */
-/*   Updated: 2019/02/20 15:14:23 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/02/21 12:05:24 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,16 @@
 #include "dispatcher.h"
 #include "libft.h"
 #include "prs_struct.h"
+#include "struct_disp.h"
 
 static t_ret	g_ret;
 
 int	printf_prs(char **ret, const char *format, va_list ap)
 {
-	void	(*f)();
+	t_specifier	s_functions;
 
+	s_functions.f = NULL;
+	s_functions.wrapper = NULL;
 	g_ret.ret = (char*)ret;
 	g_ret.i = -1;
 	while (format[++g_ret.i])
@@ -30,8 +33,9 @@ int	printf_prs(char **ret, const char *format, va_list ap)
 		if (format[g_ret.i] == '%')
 			if (format[g_ret.i + 1])
 			{
-				if ((f = dispatcher(format[g_ret.i + 1])))
-					f(va_arg(ap, typeof(ap)));
+				s_functions = dispatcher(format[g_ret.i + 1]);
+				if ((s_functions.f))
+					s_functions.f(va_arg(ap, typeof(ap)));
 				while (format[g_ret.i])
 					++g_ret.i;
 			}
