63a0086a3ca0780979d38d7265c1a40a400b3c54
Ant0wan <abarthel@student.42.fr>
2019-02-21 15:52:08 +0100

    no printf

--- libftprintf/wrappers/passivewrap.c
+++ libftprintf/wrappers/passivewrap.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 10:38:50 by abarthel          #+#    #+#             */
-/*   Updated: 2019/02/21 15:17:14 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/02/21 15:51:51 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,9 @@
 
 #include "prs_struct.h"
 
-#include <stdio.h>
-
 extern t_ret	g_ret;
 
 void	passivewrap(void *(*f)(), va_list ap)
 {
-	printf("g_ret.i: %d\n", g_ret.i);
 	f(va_arg(ap, typeof(ap)));
 }
