29831e6be18a5c5bfa895ba7732594f27e3d94cf
Ant0wan <abarthel@student.42.fr>
2019-03-01 16:56:27 +0100

    on the go for expanding

--- libftprintf/ft_printf_prs.c
+++ libftprintf/ft_printf_prs.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 12:39:43 by abarthel          #+#    #+#             */
-/*   Updated: 2019/03/01 16:18:31 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/03/01 16:54:00 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -141,6 +141,8 @@ int			printf_prs(const char *format, va_list ap)
 		g_flags.space = 0;
 		g_flags.plus = 0;
 		g_flags.apost = 0;
+		if (g_ret.i + 1 >= g_ret.max)
+			ft_expand_ret(2);
 		g_ret.ret[++g_ret.i] = format[g_ret.fmt_i];
 	}
 	return (g_ret.i + 1);
