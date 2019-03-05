d75ec38d4c8086b8e42c181ae5a3f4c61f73ca46
Ant0wan <abarthel@student.42.fr>
2019-03-05 18:33:01 +0100

    go for encoding in wchar ls

--- libftprintf/wrappers/cs_wrap.c
+++ libftprintf/wrappers/cs_wrap.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 10:38:50 by abarthel          #+#    #+#             */
-/*   Updated: 2019/03/05 16:26:42 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/03/05 18:30:48 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,31 +26,16 @@
 
 extern t_ret		g_ret;
 extern t_options	g_options;
-extern t_modifier	g_modifier;
 extern _Bool		g_error;
 
 void	cs_wrap(void *(*f)(), va_list ap)
 {
-	wchar_t	*ws;
 	int		i;
 	int		rubbish;
 
 	i = 0;
 	while (++i < g_options.val_dol)
 		rubbish = va_arg(ap, int);
-	if (g_modifier.l)
-	{
-		ws = (wchar_t*)va_arg(ap, typeof(ap));
-		i = -1;
-		while (ws[++i])
-			if (ws[i] > 0x00FF)
-			{
-				g_error = 1;
-				return ;
-			}
-		f(ws);
-	}
-	else
-		f(va_arg(ap, typeof(ap)));
+	f(va_arg(ap, typeof(ap)));
 	++g_options.i_ap;
 }
