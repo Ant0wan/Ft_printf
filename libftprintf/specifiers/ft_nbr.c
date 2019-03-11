7c07f348cd642023e292fdacf2c5d34d66ffe0c2
Ant0wan <abarthel@student.42.fr>
2019-03-11 16:27:46 +0100

    ok for signed int

--- libftprintf/specifiers/ft_nbr.c
+++ libftprintf/specifiers/ft_nbr.c
@@ -6,33 +6,48 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 14:39:26 by abarthel          #+#    #+#             */
-/*   Updated: 2019/03/11 13:18:51 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/03/11 16:26:23 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdint.h>
+#include <stddef.h>
 
 #include "prs_struct.h"
 #include "retwriter.h"
 #include "libft.h"
 
-#define MAX_DIGITS_NB 128
+#define MAX_DIGITS_NB 64
 
-#include <stdio.h>
+extern t_modifier	g_modifier;
 
 static inline void	ft_cast_nbr(intmax_t *nb)
 {
-	if (1)
+	if (g_modifier.hh)
+		*nb = (signed char)(*nb);
+	else if (g_modifier.h)
+		*nb = (short)(*nb);
+	else if (g_modifier.l)
+		*nb = (long)(*nb);
+	else if (g_modifier.ll)
+		*nb = (long long)*nb;
+	else if (g_modifier.j)
+		*nb = (intmax_t)(*nb);
+	else if (g_modifier.t)
+		*nb = (ptrdiff_t)(*nb);
+	else if (g_modifier.z)
+		*nb = (size_t)(*nb);
+	else
 		*nb = (int)(*nb);
 }
 
 void	ft_nbr(intmax_t nb)
 {
 	char		string[MAX_DIGITS_NB];
-	int			i;
-	int			len;
-	intmax_t	cp;
+	size_t		i;
+	size_t		len;
+	uintmax_t	cp;
 
 	ft_cast_nbr(&nb);
 	cp = nb < 0 ? nb * -1 : nb;
@@ -46,7 +61,7 @@ void	ft_nbr(intmax_t nb)
 	cp = nb < 0 ? nb * -1 : nb;
 	while (cp)
 	{
-		string[--i] = (cp % 10) ^ 0x30;
+		string[--i] = ((cp % 10) ^ 0x30);
 		cp = (cp - cp % 10) / 10;
 	}
 	retwriter(string, len);
