c0412d88337f599b9627c5babd55334b9553be53
Antoine BARTHELEMY <abarthel@e1r5p3.42.fr>
2019-03-12 12:47:16 +0100

    precision on str ok

--- libftprintf/specifiers/retwriter.c
+++ libftprintf/specifiers/retwriter.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/28 11:48:50 by abarthel          #+#    #+#             */
-/*   Updated: 2019/03/11 17:10:17 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/03/12 12:45:40 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,12 +23,12 @@ extern t_options	g_options;
 extern t_flags		g_flags;
 extern _Bool		g_error;
 
-static inline void	ft_strcpy_special(char *dst, const char *src)
+static inline void	ft_strcpy_special(char *dst, const char *src, int size)
 {
 	int	count;
 
 	count = -1;
-	while (src[++count])
+	while (src[++count] && count < size)
 		dst[count] = src[count];
 	g_ret.i += count - 1;
 }
@@ -38,21 +38,11 @@ extern inline void	retwriter(char *str, int size)
 	int	width_diff;
 
 	width_diff = 0;
-	if (g_ret.i + size + g_options.precision >= g_ret.max)
-	{
-		ft_expand_ret(size < g_options.precision ? g_options.precision : size);
-		if (g_error)
-		{
-			return ;
-		}
-	}
 	if (g_ret.i + size + g_options.width >= g_ret.max)
 	{
 		ft_expand_ret(size < g_options.width ? g_options.width : size);
 		if (g_error)
-		{
 			return ;
-		}
 	}
 	if (g_flags.minus == 0)
 	{
@@ -61,11 +51,11 @@ extern inline void	retwriter(char *str, int size)
 			++width_diff;
 			g_ret.ret[++g_ret.i] = ' ';
 		}
-		ft_strcpy_special(&g_ret.ret[++g_ret.i], str);
+		ft_strcpy_special(&g_ret.ret[++g_ret.i], str, size);
 	}
 	else if (g_flags.minus == 1)
 	{
-		ft_strcpy_special(&g_ret.ret[++g_ret.i], str);
+		ft_strcpy_special(&g_ret.ret[++g_ret.i], str, size);
 		while (size + width_diff < g_options.width)
 		{
 			++width_diff;
