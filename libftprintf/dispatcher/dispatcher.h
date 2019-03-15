33dca1e41ebcb0d76a417af0084f2b645f09050e
Ant0wan <abarthel@student.42.fr>
2019-03-15 10:14:57 +0100

    ok for the dispatcher light

--- libftprintf/dispatcher/dispatcher.h
+++ libftprintf/dispatcher/dispatcher.h
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 14:46:43 by abarthel          #+#    #+#             */
-/*   Updated: 2019/03/13 11:29:58 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/03/15 10:14:16 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,6 @@
 # define FT_DISPATCHER_H
 
 # include "struct_disp.h"
-
-# ifndef NB_PTR
-#  define NB_PTR 28
-# endif
 
 void	*dispatcher(char c);
 
