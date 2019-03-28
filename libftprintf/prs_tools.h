134eba216c6ad5a29f7a5f607c0bc05031550ee4
Ant0wan <abarthel@student.42.fr>
2019-03-28 12:56:17 +0100

    going light

--- libftprintf/prs_tools.h
+++ libftprintf/prs_tools.h
@@ -6,18 +6,12 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/27 15:51:04 by abarthel          #+#    #+#             */
-/*   Updated: 2019/03/28 12:51:12 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/03/28 12:55:50 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PRS_TOOLS_H
 # define PRS_TOOLS_H
-
-# include "prs_struct.h"
-
-extern t_ret	g_ret;
-
-void	reset_globals(void) __attribute__((always_inline));
 
 int		ft_atoi_special(const char *str) __attribute__((always_inline));
 
