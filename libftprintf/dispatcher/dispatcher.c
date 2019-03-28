5ab10ce7c363853cba607be3d0da3eded9df04b6
Ant0wan <abarthel@student.42.fr>
2019-03-28 12:09:01 +0100

    going light

--- libftprintf/dispatcher/dispatcher.c
+++ libftprintf/dispatcher/dispatcher.c
@@ -6,17 +6,14 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 14:51:32 by abarthel          #+#    #+#             */
-/*   Updated: 2019/03/25 14:25:04 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/03/28 12:08:23 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-#include "specifiers.h"
 #include "struct_disp.h"
-#include "prs_tools.h"
-#include "libft.h"
-#include "ft_wchar.h"
+#include "specifiers.h"
 
 #ifndef T_
 # define T_ (void*)
@@ -28,33 +25,33 @@
 
 static const t_specifier	g_specifier[] =
 {
-	{ 'c', T_ & ft_chr}, // OK
-	{ 'd', T_ & ft_nbr}, // OK
-	{ 'e', T_ & ft_putnbr},
-	{ 'f', T_ & ft_putnbr},
-	{ 'i', T_ & ft_nbr}, // OK
-	{ 'o', T_ & ft_o}, // KO
-	{ 's', T_ & ft_str}, // OK
-	{ 'u', T_ & ft_unbr}, // OK
-	{ 'x', T_ & ft_xlow}, // OK
-	{ 'X', T_ & ft_xupp}, // OK
-	{ 'C', T_ & ft_wchr}, // not all OK
-	{ 'p', T_ & ft_p}, // KO
-	{ 'n', T_ & ft_n}, // OK
-	{ 'D', T_ & ft_putuint},
+	{ 'c', T_ & ft_chr},
+	{ 'd', T_ & ft_nbr},
+	{ 'e', T_ & ft_nbr},
+	{ 'f', T_ & ft_nbr},
+	{ 'i', T_ & ft_nbr},
+	{ 'o', T_ & ft_o},
+	{ 's', T_ & ft_str},
+	{ 'u', T_ & ft_unbr},
+	{ 'x', T_ & ft_xlow},
+	{ 'X', T_ & ft_xupp},
+	{ 'C', T_ & ft_wchr},
+	{ 'p', T_ & ft_p},
+	{ 'n', T_ & ft_n},
+	{ 'D', T_ & ft_unbr},
 	{ 'O', T_ & ft_o},
-	{ 'U', T_ & ft_uold}, // OK
-	{ 'E', T_ & ft_putaddr},
-	{ 'F', T_ & ft_putaddr},
-	{ 'g', T_ & ft_putaddr},
-	{ 'G', T_ & ft_putaddr},
-	{ 'a', T_ & ft_putaddr},
-	{ 'A', T_ & ft_putaddr},
-	{ 'S', T_ & ft_wstr}, // not all OK
-	{ 'k', T_ & ft_putaddr}, // date in ISO
-	{ 'r', T_ & ft_putaddr}, // non printable character print
-	{ 'b', T_ & ft_printbits}, // binary print of the characterm use modifiers for the length
-	{ 'y', T_ & ft_printbits} // wild cards should works, modifiers are Bold/italic.. width is foreground color, and precision is background color, what about options ? try to use forum marksup
+	{ 'U', T_ & ft_uold},
+	{ 'E', T_ & ft_p},
+	{ 'F', T_ & ft_p},
+	{ 'g', T_ & ft_p},
+	{ 'G', T_ & ft_p},
+	{ 'a', T_ & ft_p},
+	{ 'A', T_ & ft_p},
+	{ 'S', T_ & ft_wstr},
+	{ 'k', T_ & ft_p},
+	{ 'r', T_ & ft_p},
+	{ 'b', T_ & ft_p},
+	{ 'y', T_ & ft_p}
 };
 
 void	*dispatcher(char c)
@@ -62,9 +59,9 @@ void	*dispatcher(char c)
 	char	i;
 
 	i = 0;
-	while (i < NB_PTR && g_specifier[(int)i].type != c)
+	while (i < NB_PTR && g_specifier[(int)i].type ^ c)
 		++i;
-	if (i == NB_PTR)
+	if (!(i ^ NB_PTR))
 		return (NULL);
 	else
 		return (g_specifier[(int)i].f);
