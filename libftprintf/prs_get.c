5aac5ab4042455bce1bd89732e04840e3ced4578
Ant0wan <abarthel@student.42.fr>
2019-04-03 19:23:26 +0200

    going to finish nbr

--- libftprintf/prs_get.c
+++ libftprintf/prs_get.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/28 12:17:11 by abarthel          #+#    #+#             */
-/*   Updated: 2019/04/01 21:48:36 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/04/03 19:08:22 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -144,4 +144,5 @@ extern inline void		get_precision(const char *format, va_list ap)
 			g_options.precision = 0;
 		}
 	}
+	g_flags.zero = 0; // added to check the condition
 }
