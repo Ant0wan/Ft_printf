8d3fd87ae4830fb0b31e1fdd43281bdce3d6878d
Ant0wan <abarthel@student.42.fr>
2019-04-04 15:04:57 +0200

    adding bases

--- libftprintf/specifiers/prefix.c
+++ libftprintf/specifiers/prefix.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 14:27:24 by abarthel          #+#    #+#             */
-/*   Updated: 2019/04/04 14:48:39 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/04/04 15:01:35 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,5 @@
 
 #include "prefix.h"
 
-t_prefix	g_prefix = {.base = 10, .size = 0, .prefix = NULL};
+t_prefix	g_prefix =
+{.base = 10, .size = 0, .prefix = NULL, .ch_base = "0123456789ABCDEF"};
