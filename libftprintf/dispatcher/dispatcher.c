c76ff21bfd04f43acc7cd280c50221f0b59720eb
Ant0wan <abarthel@student.42.fr>
2019-04-05 15:03:34 +0200

    going to put len in struc

--- libftprintf/dispatcher/dispatcher.c
+++ libftprintf/dispatcher/dispatcher.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/15 14:51:32 by abarthel          #+#    #+#             */
-/*   Updated: 2019/04/04 16:56:13 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/04/05 15:02:48 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ static const t_specifier	g_specifier[] =
 	{ 'e', T_ & ft_nbr},
 	{ 'f', T_ & ft_nbr},
 	{ 'i', T_ & ft_nbr},
-	{ 'o', T_ & ft_octa},
+	{ 'o', T_ & ft_lhexa},
 	{ 's', T_ & ft_nbr},
 	{ 'u', T_ & ft_unbr},
 	{ 'x', T_ & ft_lhexa},
@@ -50,7 +50,7 @@ static const t_specifier	g_specifier[] =
 	{ 'S', T_ & ft_nbr},
 	{ 'k', T_ & ft_nbr},
 	{ 'r', T_ & ft_nbr},
-	{ 'b', T_ & ft_bina},
+	{ 'b', T_ & ft_nbr},
 	{ 'y', T_ & ft_nbr}
 };
 
--- libftprintf/specifiers/ft_nbr.c
+++ libftprintf/specifiers/ft_nbr.c
@@ -6,42 +6,19 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 14:39:26 by abarthel          #+#    #+#             */
-/*   Updated: 2019/04/04 16:22:53 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/04/05 14:58:39 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stddef.h>
-#include <limits.h>
 
 #include "prs_struct.h"
-#include "ft_expand_ret.h"
 #include "prefix.h"
+#include "cast.h"
 #include "format.h"
 
-extern t_modifier	g_modifier;
 extern t_flags		g_flags;
 extern t_prefix		g_prefix;
-
-static inline void				ft_cast_nbr(intmax_t *nb)
-{
-	if (g_modifier.hh)
-		*nb = (signed char)(*nb);
-	else if (g_modifier.h)
-		*nb = (short)(*nb);
-	else if (g_modifier.l)
-		*nb = (long)(*nb);
-	else if (g_modifier.ll)
-		*nb = (long long)*nb;
-	else if (g_modifier.j)
-		*nb = (intmax_t)(*nb);
-	else if (g_modifier.t)
-		*nb = (ptrdiff_t)(*nb);
-	else if (g_modifier.z)
-		*nb = (size_t)(*nb);
-	else
-		*nb = (int)(*nb);
-}
 
 void							ft_nbr(intmax_t nb)
 {
