4264d96cf08bf65df13b76b2f3a137b704566832
Ant0wan <abarthel@student.42.fr>
2019-04-05 16:56:50 +0200

    ok for o x X d i u

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jmartel <jmartel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/13 10:46:53 by jmartel           #+#    #+#             */
-/*   Updated: 2019/04/05 15:59:51 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/04/05 16:53:42 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 int			main(void)
 {
 	int ret;
-	int		nb = 12;
+	int		nb = 0;
 	ret = ft_printf("%o", nb);
 	printf("|%d\n", ret);
 	ret = printf("%o", nb);
@@ -94,5 +94,121 @@ int			main(void)
 	ret = printf("size + hash + minus + prec:%-#7.3o", nb);
 	printf("|%d\n", ret);
 	ret = printf("");
+	nb = 0;
+	ft_printf("null prec : %.0x\n", nb);
+	dprintf(2, "null prec : %.0x\n", nb);
+	ft_printf("null prec : %.x\n", nb);
+	dprintf(2, "null prec : %.x\n", nb);
+	ft_printf("null prec : %12.0x\n", nb);
+	dprintf(2, "null prec : %12.0x\n", nb);
+	ft_printf("null prec : %-5.x\n", nb);
+	dprintf(2, "null prec : %-5.x\n", nb);
+
+	ft_printf("null prec : %#.0x\n", nb);
+	dprintf(2, "null prec : %#.0x\n", nb);
+	ft_printf("null prec : %#.x\n", nb);
+	dprintf(2, "null prec : %#.x\n", nb);
+	ft_printf("null prec : %#12.0x\n", nb);
+	dprintf(2, "null prec : %#12.0x\n", nb);
+	ft_printf("null prec : %#-5.x\n", nb);
+	dprintf(2, "null prec : %#-5.x\n", nb);
+
+	nb = 0;
+	ft_printf("null prec : %.0X\n", nb);
+	dprintf(2,"null prec : %.0X\n", nb);
+	ft_printf("null prec : %.X\n", nb);
+	dprintf(2,"null prec : %.X\n", nb);
+	ft_printf("null prec : %12.0X\n", nb);
+	dprintf(2,"null prec : %12.0X\n", nb);
+	ft_printf( "null prec : %-5.X\n", nb);
+	dprintf(2,"null prec : %-5.X\n", nb);
+
+	ft_printf("null prec : %#.0X\n", nb);
+	dprintf(2, "null prec : %#.0X\n", nb);
+	ft_printf("null prec : %#.X\n", nb);
+	dprintf(2, "null prec : %#.X\n", nb);
+	ft_printf("null prec : %#12.0X\n", nb);
+	dprintf(2, "null prec : %#12.0X\n", nb);
+	ft_printf("null prec : %#-5.X\n", nb);
+	dprintf(2, "null prec : %#-5.X\n", nb);
+
+	nb = 0;
+	ft_printf("null prec : %.0o\n", nb);
+	dprintf(2,"null prec : %.0o\n", nb);
+	ft_printf("null prec : %.o\n", nb);
+	dprintf(2,"null prec : %.o\n", nb);
+	ft_printf("null prec : %12.0o\n", nb);
+	dprintf(2,"null prec : %12.0o\n", nb);
+	ft_printf( "null prec : %-5.o\n", nb);
+	dprintf(2,"null prec : %-5.o\n", nb);
+
+	ft_printf(".0null prec : %#.0o\n", nb);
+	dprintf(2, ".0null prec : %#.0o\n", nb);
+	ft_printf(".null prec : %#.o\n", nb);
+	dprintf(2, ".null prec : %#.o\n", nb);
+	ft_printf("12.0null prec : %#12.0o\n", nb);
+	dprintf(2, "12.0null prec : %#12.0o\n", nb);
+	ft_printf("-5.null prec : %#-5.o\n", nb);
+	dprintf(2, "-5.null prec : %#-5.o\n", nb);
+
+	nb = 21;
+	ft_printf("null prec : %.0x\n", nb);
+	dprintf(2, "null prec : %.0x\n", nb);
+	ft_printf("null prec : %.x\n", nb);
+	dprintf(2, "null prec : %.x\n", nb);
+	ft_printf("null prec : %12.0x\n", nb);
+	dprintf(2, "null prec : %12.0x\n", nb);
+	ft_printf("null prec : %-5.x\n", nb);
+	dprintf(2, "null prec : %-5.x\n", nb);
+
+	nb = 21;
+	ft_printf("null prec : %.0X\n", nb);
+	dprintf(2,"null prec : %.0X\n", nb);
+	ft_printf("null prec : %.X\n", nb);
+	dprintf(2,"null prec : %.X\n", nb);
+	ft_printf("null prec : %12.0X\n", nb);
+	dprintf(2,"null prec : %12.0X\n", nb);
+	ft_printf( "null prec : %-5.X\n", nb);
+	dprintf(2,"null prec : %-5.X\n", nb);
+
+	nb = 21;
+	ft_printf("null prec : %.0o\n", nb);
+	dprintf(2,"null prec : %.0o\n", nb);
+	ft_printf("null prec : %.o\n", nb);
+	dprintf(2,"null prec : %.o\n", nb);
+	ft_printf("null prec : %12.0o\n", nb);
+	dprintf(2,"null prec : %12.0o\n", nb);
+	ft_printf( "null prec : %-5.o\n", nb);
+	dprintf(2,"null prec : %-5.o\n", nb);
+
+	nb = -543;
+	ft_printf("null prec : %.0x\n", nb);
+	dprintf(2, "null prec : %.0x\n", nb);
+	ft_printf("null prec : %.x\n", nb);
+	dprintf(2, "null prec : %.x\n", nb);
+	ft_printf("null prec : %12.0x\n", nb);
+	dprintf(2, "null prec : %12.0x\n", nb);
+	ft_printf("null prec : %-5.x\n", nb);
+	dprintf(2, "null prec : %-5.x\n", nb);
+
+	nb = -543;
+	ft_printf("null prec : %.0X\n", nb);
+	dprintf(2,"null prec : %.0X\n", nb);
+	ft_printf("null prec : %.X\n", nb);
+	dprintf(2,"null prec : %.X\n", nb);
+	ft_printf("null prec : %12.0X\n", nb);
+	dprintf(2,"null prec : %12.0X\n", nb);
+	ft_printf( "null prec : %-5.X\n", nb);
+	dprintf(2,"null prec : %-5.X\n", nb);
+
+	nb = -543;
+	ft_printf("null prec : %.0o\n", nb);
+	dprintf(2,"null prec : %.0o\n", nb);
+	ft_printf("null prec : %.o\n", nb);
+	dprintf(2,"null prec : %.o\n", nb);
+	ft_printf("null prec : %12.0o\n", nb);
+	dprintf(2,"null prec : %12.0o\n", nb);
+	ft_printf( "null prec : %-5.o\n", nb);
+	dprintf(2,"null prec : %-5.o\n", nb);
 	return (0);
 }
