29e2e13313419fa0a004cffae33504178f32fa76
Ant0wan <abarthel@student.42.fr>
2019-04-08 14:16:05 +0200

    norme on the go

--- libftprintf/specifiers/ft_p.c
+++ libftprintf/specifiers/ft_p.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/05 17:00:19 by abarthel          #+#    #+#             */
-/*   Updated: 2019/04/05 17:26:07 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/04/08 14:14:35 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 #include "prs_struct.h"
 #include "specifiers.h"
 
-#ifndef x64BYTES
-# define x64BYTES 8
+#ifndef X64BYTES
+# define X64BYTES 8
 #endif
 
 extern t_flags		g_flags;
@@ -26,7 +26,7 @@ extern t_modifier	g_modifier;
 void	ft_p(void *ptr)
 {
 	g_flags.hash = 1;
-	if (sizeof(void*) == x64BYTES)
+	if (sizeof(void*) == X64BYTES)
 		g_modifier.l = 1;
 	ft_lhexa((intmax_t)ptr);
 }
