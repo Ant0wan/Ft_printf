9dc2b3bb9bc923d4cf5e30cdc507d5dac48faf37
Ant0wan <abarthel@student.42.fr>
2019-04-08 14:10:44 +0200

    new version

--- libftprintf/specifiers/ft_hexa.c
+++ libftprintf/specifiers/ft_hexa.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 14:39:26 by abarthel          #+#    #+#             */
-/*   Updated: 2019/04/05 19:49:03 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/04/08 14:07:55 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,12 @@ void							ft_lhexa(uintmax_t nb)
 	reset_prefix();
 	ft_cast_unbr(&nb);
 	g_prefix.base = 16;
-	g_prefix.size = 2;
 	g_flags.plus = 0;
 	if (g_flags.hash && nb > 0)
+	{
+		g_prefix.size = 2;
 		g_prefix.prefix = "0x";
-	else
-		g_prefix.size = 0;
+	}
 	g_prefix.ch_base = "0123456789abcdef";
 	g_prefix.len = ft_nbrlen((intmax_t)nb);
 	format(nb);
@@ -41,13 +41,12 @@ void							ft_uhexa(uintmax_t nb)
 	reset_prefix();
 	ft_cast_unbr(&nb);
 	g_prefix.base = 16;
-	g_prefix.size = 2;
 	g_flags.plus = 0;
 	if (g_flags.hash && nb > 0)
+	{
+		g_prefix.size = 2;
 		g_prefix.prefix = "0X";
-	else
-		g_prefix.size = 0;
-	g_prefix.ch_base = "0123456789ABCDEF";
+	}
 	g_prefix.len = ft_nbrlen((intmax_t)nb);
 	format(nb);
 }
--- libftprintf/specifiers/ft_unbr.c
+++ libftprintf/specifiers/ft_unbr.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/03 20:01:40 by abarthel          #+#    #+#             */
-/*   Updated: 2019/04/05 19:48:21 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/04/08 14:02:50 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,9 +29,5 @@ void	ft_unbr(uintmax_t nb)
 	g_flags.hash = 0;
 	g_flags.space = 0;
 	g_flags.plus = 0;
-	g_prefix.base = 10;
-	g_prefix.size = 0;
-	g_prefix.neg = 0;
-	g_prefix.ch_base = "0123456789ABCDEF";
 	format(nb);
 }
