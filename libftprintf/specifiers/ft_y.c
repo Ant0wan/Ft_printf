a37c906c8a3ebb5df78d596cd7de806b20c37096
Ant0wan <abarthel@student.42.fr>
2019-04-09 14:21:12 +0200

    going to wchar

--- libftprintf/specifiers/ft_y.c
+++ libftprintf/specifiers/ft_y.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/08 17:38:39 by abarthel          #+#    #+#             */
-/*   Updated: 2019/04/08 17:57:45 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/04/09 14:19:41 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,10 @@
 extern t_flags		g_flags;
 extern t_modifier	g_modifier;
 extern t_options	g_options;
+
+/*
+** ANSI/VT100 compliant
+*/
 
 void	ft_y(void)
 {
@@ -28,7 +32,7 @@ void	ft_y(void)
 		g_ret.ret[++g_ret.i] = '0';
 		g_ret.ret[++g_ret.i] = 'm';
 	}
-	else
+	if (g_options.width)
 	{
 		g_ret.ret[++g_ret.i] = '\e';
 		g_ret.ret[++g_ret.i] = '[';
@@ -37,8 +41,48 @@ void	ft_y(void)
 		g_ret.ret[++g_ret.i] = ';';
 		g_ret.ret[++g_ret.i] = '5';
 		g_ret.ret[++g_ret.i] = ';';
+		// here value foreground
+		g_ret.ret[++g_ret.i] = 'm';
+	}
+	if (g_options.precision >= 0)
+	{
+		g_ret.ret[++g_ret.i] = '\e';
+		g_ret.ret[++g_ret.i] = '[';
+		g_ret.ret[++g_ret.i] = '4';
 		g_ret.ret[++g_ret.i] = '8';
+		g_ret.ret[++g_ret.i] = ';';
+		g_ret.ret[++g_ret.i] = '5';
+		g_ret.ret[++g_ret.i] = ';';
+		// here value background
+		g_ret.ret[++g_ret.i] = 'm';
+	}
+	if (g_flags.hash) //bold
+	{
+		g_ret.ret[++g_ret.i] = '\e';
+		g_ret.ret[++g_ret.i] = '[';
+		g_ret.ret[++g_ret.i] = '1';
+		g_ret.ret[++g_ret.i] = 'm';
+	}
+	if (g_flags.minus) // faint
+	{
+		g_ret.ret[++g_ret.i] = '\e';
+		g_ret.ret[++g_ret.i] = '[';
 		g_ret.ret[++g_ret.i] = '2';
 		g_ret.ret[++g_ret.i] = 'm';
 	}
+	if (g_flags.plus) // italic
+	{
+		g_ret.ret[++g_ret.i] = '\e';
+		g_ret.ret[++g_ret.i] = '[';
+		g_ret.ret[++g_ret.i] = '3';
+		g_ret.ret[++g_ret.i] = 'm';
+	}
+	if (g_flags.apost) // underline
+	{
+		g_ret.ret[++g_ret.i] = '\e';
+		g_ret.ret[++g_ret.i] = '[';
+		g_ret.ret[++g_ret.i] = '4';
+		g_ret.ret[++g_ret.i] = 'm';
+	}
+
 }
