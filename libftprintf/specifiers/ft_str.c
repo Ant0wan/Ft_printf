9b472d3ff9e4fb2b54bdde97dddd297539c25d0c
Ant0wan <abarthel@student.42.fr>
2019-04-10 13:57:33 +0200

    changed file format for str

--- libftprintf/specifiers/ft_str.c
+++ libftprintf/specifiers/ft_str.c
@@ -6,62 +6,27 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 14:39:26 by abarthel          #+#    #+#             */
-/*   Updated: 2019/04/10 12:57:05 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/04/10 13:54:58 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
 #include "prs_struct.h"
-#include "ft_expand_ret.h"
 #include "prefix.h"
 #include "libft.h"
 #include "ft_wchar.h"
+#include "str_format.h"
 
 #ifndef NULL_STR
 # define NULL_STR "(null)"
 #endif
 
-extern t_flags		g_flags;
 extern t_options	g_options;
 extern t_modifier	g_modifier;
 extern t_prefix		g_prefix;
 
-static inline void				ret_str(char *str)
-{
-	while (g_prefix.len--)
-	{
-		g_ret.ret[++g_ret.i] = *str++;
-		--g_options.width;
-	}
-}
-
-static inline void				pre_format_str(void)
-{
-	while (!(g_flags.minus) && g_options.width - g_prefix.len > 0)
-	{
-		--g_options.width;
-		g_ret.ret[++g_ret.i] = ' ';
-	}
-}
-
-static inline void	format_str(void *str)
-{
-	int	size;
-
-	size = g_options.width < g_prefix.len ? g_prefix.len : g_options.width;
-	while (g_ret.i + size >= g_ret.max)
-		ft_expand_ret(size);
-	pre_format_str();
-	ret_str(str);
-	while (g_flags.minus && g_options.width > 0)
-	{
-		--g_options.width;
-		g_ret.ret[++g_ret.i] = ' ';
-	}
-}
-
-void				ft_str(void	*str)
+void	ft_str(void	*str)
 {
 	reset_prefix();
 	if (!(str))
@@ -72,10 +37,10 @@ void				ft_str(void	*str)
 		g_prefix.len = ft_strlen((char*)str);
 	if (g_options.precision > -1 && g_options.precision < g_prefix.len)
 		g_prefix.len = g_options.precision;
-	format_str(str);
+	str_format(str);
 }
 
-void				ft_wstr(void *str)
+void	ft_wstr(void *str)
 {
 	g_modifier.l = 1;
 	ft_str(str);
---
+++ libftprintf/specifiers/str_format.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   str_format.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/12 14:39:26 by abarthel          #+#    #+#             */
+/*   Updated: 2019/04/10 13:57:09 by abarthel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+#include "prs_struct.h"
+#include "ft_expand_ret.h"
+#include "prefix.h"
+
+extern t_flags		g_flags;
+extern t_options	g_options;
+extern t_modifier	g_modifier;
+extern t_prefix		g_prefix;
+
+static inline void				ret_str(char *str)
+{
+	while (g_prefix.len--)
+	{
+		g_ret.ret[++g_ret.i] = *str++;
+		--g_options.width;
+	}
+}
+
+static inline void				ret_wcs(char *str)
+{
+	while (g_prefix.len--)
+	{
+		g_ret.ret[++g_ret.i] = *str++;
+		--g_options.width;
+	}
+}
+
+static inline void				pre_format_str(void)
+{
+	while (!(g_flags.minus) && g_options.width - g_prefix.len > 0)
+	{
+		--g_options.width;
+		g_ret.ret[++g_ret.i] = ' ';
+	}
+}
+
+extern inline void				str_format(void *str)
+{
+	int	size;
+
+	size = g_options.width < g_prefix.len ? g_prefix.len : g_options.width;
+	while (g_ret.i + size >= g_ret.max)
+		ft_expand_ret(size);
+	pre_format_str();
+	if (g_modifier.l)
+		ret_wcs(str);
+	else
+		ret_str(str);
+	while (g_flags.minus && g_options.width > 0)
+	{
+		--g_options.width;
+		g_ret.ret[++g_ret.i] = ' ';
+	}
+}
