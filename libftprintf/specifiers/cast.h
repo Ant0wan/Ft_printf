de885a91e08e37f11c2f2f6945c45804a741a7c2
Ant0wan <abarthel@student.42.fr>
2019-04-18 12:32:59 +0200

    put perf on all, but no O3 cheat

--- libftprintf/specifiers/cast.h
+++ libftprintf/specifiers/cast.h
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/05 14:52:15 by abarthel          #+#    #+#             */
-/*   Updated: 2019/04/17 10:59:47 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/04/18 12:17:14 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,10 @@
 
 # include <stdlib.h>
 
-void	ft_cast_nbr(intmax_t *nb) __attribute__((always_inline));
+void	ft_cast_nbr(intmax_t *restrict nb) __attribute__
+		((nonnull,always_inline));
 
-void	ft_cast_unbr(uintmax_t *nb) __attribute__((always_inline));
+void	ft_cast_unbr(uintmax_t *restrict nb) __attribute__
+		((nonnull,always_inline));
 
 #endif
