ad90425f11fdac30ba765858e2efce568cd686e0
Ant0wan <abarthel@student.42.fr>
2019-04-19 18:14:17 +0200

    ok for __attribute__((packed))

--- libftprintf/specifiers/grisu.h
+++ libftprintf/specifiers/grisu.h
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/19 10:20:08 by abarthel          #+#    #+#             */
-/*   Updated: 2019/04/19 17:04:44 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/04/19 18:13:31 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 # define GRISU_H
 
 # include <stdint.h>
+# include <float.h>
 
 typedef struct	s_dfp
 {
-	_Bool			sign:1;
-	unsigned short	e:11;
-	uint64_t		f:53; // not sure if 52 because of hidden value 0x10000000000000
+	_Bool			s:1;
+	unsigned short	b:5 __attribute__((packed));
+	unsigned short	e:11 __attribute__((packed));
+	uint64_t		f:53 __attribute__((packed)); // not sure if 52 because of hidden value 0x10000000000000
 }				t_dfp;
 
 #endif
