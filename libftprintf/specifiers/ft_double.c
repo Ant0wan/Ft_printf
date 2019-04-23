b3e892bb0692cbc765b31ecdf0e4803b1ab61fb9
Ant0wan <abarthel@student.42.fr>
2019-04-23 14:04:19 +0200

    going to grade

--- libftprintf/specifiers/ft_double.c
+++ libftprintf/specifiers/ft_double.c
@@ -6,7 +6,7 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/12 14:39:26 by abarthel          #+#    #+#             */
-/*   Updated: 2019/04/19 17:44:38 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/04/23 13:59:09 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,14 @@
 
 #include "grisu.h"
 #include "prs_struct.h"
+#include "ft_printf.h"
 
 extern t_modifier	g_modifier;
 
-#include <stdio.h> // DEBUGG
 void							ft_double(va_list ap)
 {
-	printf("size struct:%zu\n", sizeof(t_dfp));
-	(void)ap;
 	if (g_modifier.upl)
-		printf("%Lf", va_arg(ap, long double));
+		ft_printf("%d", va_arg(ap, long double));
 	else
-		printf("%f", va_arg(ap, double));
+		ft_printf("%d", va_arg(ap, double));
 }
--- libftprintf/specifiers/grisu.c
+++ libftprintf/specifiers/grisu.c
@@ -6,21 +6,22 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/23 11:38:05 by abarthel          #+#    #+#             */
-/*   Updated: 2019/04/23 12:31:27 by abarthel         ###   ########.fr       */
+/*   Updated: 2019/04/23 14:03:19 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
+#include "libft.h"
 #include "ft_printf.h"
 #include "grisu.h"
 
-#define D_1_LOG2_10 0.30102999566398114 //  = 1 / log(10) 
+#define D_1_LOG2_10 0.30102999566398114 
 #define TEN7 10000000
 
 t_dfp	cached_power(int k)
 {
-	return ((t_dfp){.f = 0, .e = 0});
+	return ((t_dfp){.f = 0, .e = k});
 }
 
 t_dfp	minus(t_dfp x, t_dfp y)
@@ -33,40 +34,29 @@ t_dfp	minus(t_dfp x, t_dfp y)
 
 t_dfp	multiply(t_dfp x, t_dfp y)
 {
-	t_dfp		r;
 	uint64_t	a;
 	uint64_t	b;
 	uint64_t	c;
 	uint64_t	d;
-	uint64_t	ac;
-	uint64_t	bc;
-	uint64_t	ad;
-	uint64_t	bd;
 	uint64_t	tmp;
-	uint64_t	M32;
-   
-	M32	= 0xFFFFFFFF;
-   	a = x.f >> 32;
-	b = x.f & M32;
+
+	a = x.f >> 32;
+	b = x.f & 0xFFFFFFFF;
 	c = y.f >> 32;
-	d = y.f & M32;
-	ac = a * c;
-	bc = b * c;
-	ad = a * d;
-	bd = b * d;
-	tmp = (bd >> 32) + (ad & M32) + (bc & M32);
-	tmp += 1U << 31; // Round
-	r.f = ac + (ad>>32) + (bc>>32) + (tmp>>32);
-	r.e= x.e + y.e + 64;
-	return (r);
+	d = y.f & 0xFFFFFFFF;
+	tmp = ((b * d) >> 32) + ((a * d) & 0xFFFFFFFF) + ((b * c) & 0xFFFFFFFF);
+	tmp += 1U << 31;
+	return ((t_dfp)
+			{.f = (a * c) + ((a * d) >> 32) + ((b * c) >> 32) + (tmp >> 32)
+			, .e = x.e + y.e + 64});
 }
 
 int ft_simple_ceil(double v)
 {
 	int k;
-	
+
 	k = (int)v;
-    if (v < 0)
+	if (v < 0)
 		return (k);
 	return (v - k == 0 ? k : k + 1);
 }
@@ -78,10 +68,49 @@ int		k_comp(int e, int alpha)
 
 void	cut(t_dfp D, uint32_t parts[3])
 { 
-	parts[2] = (D.f % (TEN7 >> D.e)) << D.e;
 	uint64_t tmp = D.f / (TEN7 >> D.e);
+	
+	parts[2] = (D.f % (TEN7 >> D.e)) << D.e;
 	parts[1] = tmp % TEN7;
 	parts[0] = tmp / TEN7;
+}
+
+double	ft_frexp(double v, int *e)
+{
+	union
+	{
+		double d;
+		uint64_t i;
+	} y = { x };
+	int ee = y.i >> 52 & 0x7ff;
+
+	if (!ee)
+	{
+		if (x)
+		{
+			x = frexp(x*0x1p64, e);
+			*e -= 64;
+		}
+		else
+			*e = 0;
+		return x;
+	}
+	else if (ee == 0x7ff)
+		return x;
+	*e = ee - 0x3fe;
+	y.i &= 0x800fffffffffffffull;
+	y.i |= 0x3fe0000000000000ull;
+	return (y.d);
+}
+
+t_dfp	double_to_t_dfp(double v)
+{
+	
+}
+
+t_dfp	normalize(t_dfp v)
+{
+
 }
 
 void	grisu(double v, char *buffer)
@@ -90,10 +119,10 @@ void	grisu(double v, char *buffer)
 	uint32_t	ps[3];
 	int 		q = 64;
 	int			alpha = 0;
-	int			gamma = 3;
-	
-//	w = normalize_t_dfp(double2t_dfp(v)); // have to build the functiosn that normalize and tranforme before double to struct.
-//	int mk = k_comp(w.e + q, alpha, gamma);
+	//	int			gamma = 3;
+
+	//	w = normalize_t_dfp(double_to_t_dfp(v)); // have to build the functiosn that normalize and tranforme before double to struct.
+	//	int mk = k_comp(w.e + q, alpha, gamma);
 	int mk = k_comp(w.e + q, alpha);
 	t_dfp c_mk = cached_power(mk);
 	t_dfp D = multiply(w, c_mk);
